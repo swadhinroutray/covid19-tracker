c13dbc4f58f22a2d1498a13b65fb74e33a848183
swadhinroutray <swadhin.routray@gmail.com>
2020-04-24 15:17:38 +0530

    DeployCodeHeroku

---
+++ build/precache-manifest.cc880f4af3104ece3e105f9c4ff9d3df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced6a893e30263752f13bd12c79d5595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb2a0ed13c402bbce46",
+    "url": "/static/css/main.b941d894.chunk.css"
+  },
+  {
+    "revision": "0190f2b0ba2e5731afc9",
+    "url": "/static/js/2.ce5c7ea6.chunk.js"
+  },
+  {
+    "revision": "7a8493f3b4ea0d5f3151dd7af35517cf",
+    "url": "/static/js/2.ce5c7ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb2a0ed13c402bbce46",
+    "url": "/static/js/main.c0e7c8d4.chunk.js"
+  },
+  {
+    "revision": "6909ddb41416de7848ac",
+    "url": "/static/js/runtime-main.dac0035b.js"
+  },
+  {
+    "revision": "1fe4a8a117855c5f53b86d8a41594ac5",
+    "url": "/static/media/Background.1fe4a8a1.png"
+  }
+]);
